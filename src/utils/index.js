79dbf45a9445cdfb3ed49aa92d782a768e641247
Rabia Agha <79723723+agharabia@users.noreply.github.com>
2021-06-28 14:20:17 -0400

    merging the indices

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,16 @@
 const proc = require('./proc');
 const constants = require('./constants');
+const { buildNodeWebvttCues, buildNodeWebvttInput } = require('./JsonToWebvttUtils');
+const { mp3ToData } = require('./Mp3ToJsonUtils');
+const { mp4ToMemfs, memfsToMp3, removeFile } = require('./Mp4ToMp3Utils');
 
 module.exports = {
   constants,
   proc,
+  buildNodeWebvttCues,
+  buildNodeWebvttInput,
+  mp3ToData,
+  mp4ToMemfs,
+  memfsToMp3,
+  removeFile
 };
