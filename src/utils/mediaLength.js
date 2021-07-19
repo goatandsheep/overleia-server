0c3f85b18f2e8a111880b5334b9b6d34aeb513d3
Rabia Agha <79723723+agharabia@users.noreply.github.com>
2021-07-19 18:12:27 -0400

    upload is failing due to error in finding duration of input video

--- src/utils/mediaLength.js
+++ src/utils/mediaLength.js
@@ -5,11 +5,16 @@ ffmpeg.setFfprobePath(ffprobePath);
 
 // TODO: fix
 const mediaLength = (filePath) => {
+  const prom = ffmpeg.ffprobe(filePath, function(err, metadata) {
+    metadata.format.duration});
+  return prom;
+};
+  /*
   const prom = ffmpeg(filePath)
     .duration()
     .on('end');
   return prom;
-};
+  */
 
 module.exports = {
   mediaLength,
